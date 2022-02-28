86c71df41ec38ced0a6b02571d3e7183d0f821e9
Nadia Chennaf <nadia.chennaf@student.42lausanne.ch>
2022-02-28 10:19:43 +0100

    so_long.c edited with tests

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: nchennaf <nchennaf@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 15:43:53 by nchennaf          #+#    #+#             */
-/*   Updated: 2022/02/15 15:44:02 by nchennaf         ###   ########.fr       */
+/*   Updated: 2022/02/28 10:19:43 by nchennaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,36 @@ int	key_on(int key, void *param)
 	return (0);
 }
 
-int	main()
+/*int	main()
 {
-	void	*mlx_ptr;
+	void	*mlx;
 	void	*win_ptr;
 	void	*img;
+	char	*path;
+	int		img_w;
+	int		img_h;
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "The Legend of Diana");
-	img = mlx_new_image(mlx_ptr, 500, 500);
+	mlx = mlx_init();
+	win_ptr = mlx_new_window(mlx, 500, 500, "The Legend of Diana");
+	//img = mlx_new_image(mlx, 500, 500);
+	path = "./sprites/tile027.xpm";
+	img = mlx_xpm_file_to_image(mlx, path, &img_w, &img_h);
 
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 20, 0x00FF00FF);
+	//mlx_pixel_put(mlx, win_ptr, 250, 20, 0x00FF00FF);
 	mlx_key_hook(win_ptr, key_on, &win_ptr);
-	mlx_loop(mlx_ptr);
+	mlx_loop(mlx);
 	return (0);
+}
+*/
+
+int	main(void)
+{
+	void	*mlx;
+	void	*img;
+	char	*relative_path = "./sprites/tile027.xpm";
+	int		img_width;
+	int		img_height;
+
+	mlx = mlx_init();
+	img = mlx_xpm_file_to_image(mlx, relative_path, &img_width, &img_height);
 }
